6f7104c0d1098743c2a26e18c0e911ea3c42050d
Ricardo Ochoa Armenta <ricardo.ochoaarmenta@kuleuven.be>
2025-06-24 13:09:26 +0200

    Reestructuring Utils from the pulstar code, parsing functions (value, string, file) will be distributed into single modules

---
+++ pulstar/src/utils/parse_value.rs
@@ -0,0 +1,84 @@
+use super::InputKind;
+pub fn parse_from_cell (s: &str)->InputKind{
+    // Try bool
+    if let Ok(b) = s.parse::<bool>(){
+        return InputKind::BOOL(b);
+    }
+
+    // Try f64
+    if s.contains('.')||s.contains('e')||s.contains('E'){
+        if let Ok(f) = s.parse::<f64>(){
+            return InputKind::F64(f);
+        }
+    }
+
+    // Try U16
+    if let Ok(u_16) = s.parse::<u16>(){
+        return InputKind::U16(u_16);
+    }
+
+    // Try i16
+    if let Ok(i_16) = s.parse::<i16>(){
+        return InputKind::I16(i_16);
+    }
+
+    panic!("Failed to parse {} as a recognized primitive value",s);
+}
+
+
+
+#[cfg(test)]
+mod tests{
+
+    use super::{parse_from_cell,InputKind};
+
+    #[test]
+    fn parsing_cell_value_float(){
+        let s1 = String::from("1.0");
+        match parse_from_cell(&s1){
+            InputKind::F64(value)=>{
+                assert_eq!(value,1.0);
+            }
+            _=>{panic!("not a floating number")}
+        }
+    }
+
+    #[test]
+    fn parsing_cell_value_bool(){
+        let s1=String::from("true");
+        let s2 = String::from("false");
+        match parse_from_cell(&s1){
+            InputKind::BOOL(value)=>{assert_eq!(true,value);}
+            _=>{panic!("not correct bool")}
+        }
+        match parse_from_cell(&s2){
+            InputKind::BOOL(value)=>{assert_eq!(false,value);}
+            _=>{panic!("not correct bool")}
+        }
+    }
+
+    #[test]
+    fn parsing_cell_value_u16(){
+        let s1 = String::from("1");
+        match parse_from_cell(&s1){
+            InputKind::U16(value)=>{assert_eq!(1u16,value);}
+            _=>{panic!("not u16")}
+        } 
+    }
+
+    #[test]
+    fn parsing_cell_value_i16(){
+        let s1 = String::from("1");
+        let s2 = String::from("-1");
+        /*match parse_from_cell(&s1){
+            InputKind::I16(value)=>{assert_eq!(1i16,value);}
+            _=>{panic!("not i16")}
+        }*/ //This test fails, then if i16 is expected, the u16 must be casted as i16 when storing it in the inputs.
+        match parse_from_cell(&s2){
+            InputKind::I16(value)=>{assert_eq!(-1i16,value);}
+            _=>{panic!("not i16")}
+        }
+    }
+
+
+}
