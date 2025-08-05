f2a304afa9bd0ee9bc2fe730c548a1e3111ace42
Ricardo Ochoa Armenta <ricardo.ochoaarmenta@kuleuven.be>
2025-08-05 11:53:37 +0200

    Profile_dev: Starting point from reorganizing the parsing of grid files. The new version will read the intensity grid files (with a .txt extension) only once at the beginning of the program and then will perform queries over Polars::DataFrames created from reading this. This commit is thus a check point of a stable version of the profile code. Also from this point on, the optimizations, to check where are the bottlenecks will be done using profilers, namely sammy which is a profiler tool for rust based on perf

--- profile/src/intensity.rs
+++ profile/src/intensity.rs
@@ -1,22 +1,21 @@
-use polars::prelude::*;
+
+use polars::{error::ErrString, prelude::*};
 use super::*;
-use temp_name_lib::type_def::N_FLUX_POINTS;
 mod define_parameter_space;
-use std::{fmt::LowerExp, fs::File};
+use std::fs::File;
 
 /// This module contains the methods and functions to process the intensity grids. 
-/// It has two parts.
+/// It has three parts.
 /// 
-/// The first part consist of methods to produce a DataFrame that stores the file name of the relevant
+/// The first part consist of methods to produce a [DataFrame] that stores the file name of the relevant
 /// intensity grid files
 /// 
-/// The second part consist of methods to obtain a LazyFrame from the contents of the intensity grid file and compute 
+/// The second part consist of methods to obtain a [DataFrame]s from the contents of the intensity grid files 
+/// 
+/// The third part has the methods used to filter and compute intensity and continuum energy fluxes out the intensity [DataFrame]s
+/// 
 /// the Energy fluxes. 
 /// 
-
-
-
-
 /// The implementations for the profile config structure defined in this module are used to 
 /// extract a DataFrame that works as a database for the intensity grid files. 
 /// 
@@ -108,8 +107,66 @@ impl UnwrappedGrids{
             .sort(["temperature","log_gravity"], 
             SortMultipleOptions::default()
             .with_order_descending_multi([false,false]));
-
+ 
        df.unwrap()
+    }
+
+    /// This function creates an [Expr] that filters only the relevant grid files
+    /// 
+    /// ### Arguments:
+    /// * `temperatures` - a reference to a vector that contains all of the local temperatures for each surface cell
+    /// * `log_g` - a reference to a vector that contains all of the local values for log_g
+    /// * `delta_temp` - temperature spacing between grid files
+    /// * `delta_logg` - logg spacing between grid files
+    /// ### Returns:
+    /// This function returns a [Option] with the following variants:
+    /// * `Ok(Expr)` - where Expr defines the relevant temperatures and log g in the parameter space.
+    /// * 'None' - when there's an error on the computation of the local values. 
+    fn filter_grids(temperatures:&[f64],log_g:&[f64],delta_temp:f64,delta_logg:f64)->Option<Expr>{
+        
+        let max_temp = temperatures.iter().fold(0.0, |acc,x| if acc<*x {*x}else{acc});
+        let max_logg = log_g.iter().fold(0.0, |acc,x| if acc<*x {*x}else{acc});
+        let min_temp = temperatures.iter().fold(0.0, |acc,x| if acc>*x {*x}else{acc});
+        let min_logg = log_g.iter().fold(0.0, |acc,x| if acc>*x {*x}else{acc});
+
+        if max_temp*max_logg*min_temp*min_logg == 0.0{ //<--- If there's some maximum or minimum missing, filter nothing.
+            None
+        }else{
+
+            let upper_bound_temp = col("temperature").lt_eq(lit(max_temp+delta_temp));
+            let upper_bound_logg= col("log_gravity").lt_eq(lit(max_logg+delta_logg));
+            let lower_bound_temp = col("temperature").gt_eq(lit(min_temp-delta_temp));
+            let lower_bound_logg= col("log_gravity").gt_eq(lit(min_logg-delta_logg));
+        
+            let uu_expr = upper_bound_temp.clone().and(upper_bound_logg.clone());
+            let ul_expr = upper_bound_temp.and(lower_bound_logg.clone());
+            let lu_expr = lower_bound_temp.clone().and(upper_bound_logg);
+            let ll_expr = lower_bound_temp.and(lower_bound_logg);
+        
+            let combined_expr = uu_expr.and(lu_expr.and(ul_expr.and(ll_expr)));
+        
+            Some(combined_expr)
+        }
+        
+    }
+    /// This function creates a data frame with the columns "temperature","log_gravity","file name"
+    /// that are Float64, Float64, &[str] in that order. 
+    /// 
+    /// The data frame is ordered from lower to greater both in temperature and gravity. 
+    /// Only the relevant grid files from the computation will be loaded
+    fn load_gridfiles_into_dataframe(self,temperatures:&[f64],log_g:&[f64])->PolarsResult<DataFrame>{
+        let db_temp = self.temperatures.clone();
+        let db_log_g = self.log_g.clone();
+        let delta_temp = (db_temp.last().unwrap() - db_temp.get(0).unwrap())/(db_temp.len() as f64);
+        let delta_logg = (db_log_g.last().unwrap() - db_log_g.get(0).unwrap())/(db_log_g.len() as f64);
+        
+        let df = self.create_dataframe_sorted();
+
+        let lf = df.lazy();
+        
+        if let Some(param_space_expression) = Self::filter_grids(temperatures, log_g, delta_temp, delta_logg){
+            lf.filter(param_space_expression).collect()
+        }else { Err(PolarsError::InvalidOperation(ErrString::new_static("The loaded grid files are not adequate to carry on the computation."))) }
     }
 }
 
@@ -118,98 +175,7 @@ impl UnwrappedGrids{
 //------parsing intensity grids-----------
 //---------------------------------------- 
 
-/// This function selects the four points closer to
-/// (temperature,Log_g)
-/// on the parameter space whose coordinates have an associated intensity grid file
-/// 
-/// ### Arguments:
-/// * `grid_id_lf` - A polars LazyFrame that contains the information of the intensity grids.
-/// * `temperature`` - a `f64` value for the temperature on a surface cell
-/// * `log g`- a `f64` value for the logarithm of gravity on a surface cell
-/// ### Returns:
-/// * `(temperature_vec, log_g_vec, Filenames_vec)` - This is a tuple that contains three vectors where
-/// * `temperature vec` - is a vector of `f64` that contains 4 ordered temperatures of the coordinates in the parameter space
-/// * `log_g_vec` - is a vector of `f64 that contains 4 ordered values of log_g of the coordinates in the parameter space
-/// * `Filenames_vec` - is a vector of `String` that contains the paths for the 4 intensity grid files.
-pub fn get_temp_logg_filenames(grid_id_lf:LazyFrame,
-    temperature:f64,
-    log_gravity:f64)->(Vec<f64>,Vec<f64>,Vec<String>){
-    let lf_relevant = define_parameter_space::get_rectangles_in_parameter_space(grid_id_lf, temperature, log_gravity);
-    let df_relevant = lf_relevant.collect().unwrap();
 
-    let temperature_vec = extract_column_as_vectorf64("temperature", &df_relevant);
-    let log_g_vec = extract_column_as_vectorf64("log_gravity", &df_relevant);
-    let name_of_files = extract_column_as_vector_string("file name", &df_relevant);
-
-    (temperature_vec,log_g_vec , name_of_files)
-}
-
-
-/// This function extracts the intensity
-/// 
-/// ### Arguments: 
-/// * `name` - file name of the intensity grid file 
-/// * `shifted_wavelengths` - collection of doppler shifted wavelenghts from where intensity shall be queried.
-/// * `coschi` - projection of the surface cell normal onto the observer's unit position vector;
-/// ### Returns: 
-/// `(Flux, Continuum, wavelength_vec)` - a tupple containing three vectors of type `f64` where
-/// `Flux` - Is the intensity flux
-/// `Continuum` - continuus intensity flux
-/// `wavelength_vec` - is the wavelength values where the intensities are computed.
-pub fn get_flux_continuum(grid_filename:String,
-    shifted_wavelengths:&[f64],
-    coschi:f64,
-    start_computation:&std::time::Instant)->Option< (Vec<f64> , Vec<f64>, Vec<f64>)>{
-    if let Ok(lf) = read_intensity_grid_file(&grid_filename){
-
-        // if the number of flux points to be calculated is really small, it's useful to filter out some of the wavelengths from the data frame so that the queries are performed faster.
-        let lf_in_range = match filter_if_contains_wavelenghts(shifted_wavelengths){
-            Some(relevant_range) => {lf.filter(relevant_range)}
-            None => {lf}
-        };
-        let mut start = start_computation.elapsed();
-        let mut duration = start_computation.elapsed() - start;
-        println!("\n read intensity grid file and created lazyframe in {:?}", duration);
-        start =start_computation.elapsed();
-        let df_unfiltered = lf_in_range.collect().unwrap();
-        duration = start_computation.elapsed()-start;
-        println!(" created dataframe in {:?}", duration);
-        
-        start = start_computation.elapsed();
-        let df_filtered = extract_relevant_wavelengths(shifted_wavelengths, df_unfiltered);
-        duration = start_computation.elapsed()-start;
-        println!("filtered dataframe in {:?}", duration);
-
-        let lf_relevant = df_filtered.lazy();
-
-        /*lf;match shifted_wavelengths.len() < (N_FLUX_POINTS/100) as usize{
-            true => {
-                lf
-                .filter(filter_if_contains_wavelenght(&shifted_wavelengths,
-                0.2).unwrap())}
-            false => {lf}
-        };*/
-
-        // Here the lazy frame is transformed into a data frame that contains three columns: the intensity fluxes (specific, continuous) and the relevant wavelengths.
-        start = start_computation.elapsed();        
-        let df_flux = create_lf_wavelength_flux_continuum(lf_relevant,coschi).collect().unwrap();
-        duration = start_computation.elapsed()-start;
-        println!("final dataframe in {:?}", duration);
-
-        start = start_computation.elapsed();
-        let lbd_from_df = extract_column_as_vectorf64("wavelength", &df_flux);
-        let flux_from_df = extract_column_as_vectorf64("flux", &df_flux);
-        let continuum_from_df = extract_column_as_vectorf64("continuum", &df_flux);
-        duration = start_computation.elapsed()-start;
-        println!("gotten some vectors {:?}\n", duration);
-
-        Some(( flux_from_df,
-            continuum_from_df,
-            lbd_from_df))
-    }else{
-    None
-    }
-}
 
 /// This function is used to create a polars LazyFrame out of the intensity grid file in order to perform 
 /// column wise operations faster. 
@@ -251,41 +217,8 @@ pub fn get_flux_continuum(grid_filename:String,
 
 }
 
-/// This function receives a lazy frame containing the coefficients of the limb
-/// darkening law
-/// $I_{\lambda}(\mu) =a_0 \sum_{k=1}^{3} a_k \left( 1 - \mu^k  \right)$
-/// and returns another lazy frame with the wavelengths and their intensity fluxes.
-///
-/// ### Arguments:
-/// * `lf` - A lazy frame read from the intensity grid file
-/// * `coschi` - a `f64` value that is the projection of the unit surface normal onto the unit vector towards the observer.
-/// ### Returns:
-/// * `LazyFrame` - The column headers of this lazy frame are given by 
-/// * `|wavelength|flux|continuum|`
- fn create_lf_wavelength_flux_continuum(lf:LazyFrame, coschi:f64)->LazyFrame{
-    let mu = coschi.sqrt();
 
-    let bcoef = 1.0 - mu; 
-    let ccoef = 1.0 - coschi;
-    let dcoef = 1.0-mu.powi(3);
-
-    let i_lambda = col("a") 
-        + col("b") * lit(bcoef) 
-        + col("c") * lit(ccoef)
-        + col("d") * lit(dcoef);
-
-    let i_cont = col("ac") 
-        + col("bc") * lit(bcoef) 
-        + col("cc") * lit(ccoef)
-        + col("dc") * lit(dcoef);
-    
-    lf.select([
-        col("wavelength"),i_lambda.alias("flux"),
-        i_cont.alias("continuum")
-    ])
-}
-
-/// This function constructs a polars expression [Expr] that filters out wavelengths that are not close to the 
+/// This function constructs a polars expression [Expr] that filters out wavelengths that are greater than or less than
 /// observed (requested) ones. This reduces memory consumption and computation time.
 /// 
 /// ### Arguments: 
@@ -294,7 +227,7 @@ pub fn get_flux_continuum(grid_filename:String,
 /// ### Returns:
 /// *`Option<Expr>` -  where `Expr` is polars expression  that filters out unrelevant wavelengths from the lazyframe of an intensity grid file.
 /// 
-fn filter_if_contains_wavelenghts(shifted_wavelengths:&[f64])->Option<Expr>{
+fn filter1_if_contains_wavelenghts(shifted_wavelengths:&[f64])->Option<Expr>{
 
     let max_wavelength = shifted_wavelengths.last().unwrap().clone();
     let min_wavelength = shifted_wavelengths.get(0).unwrap().clone();
@@ -306,7 +239,9 @@ fn filter_if_contains_wavelenghts(shifted_wavelengths:&[f64])->Option<Expr>{
     Some(combined_filter_exp)
 }
 
-/// This is a function that extracts only the relevant wavelengths. 
+
+
+/// This is a function that extracts only the relevant wavelengths as [Vec<f64>]. 
 /// 
 /// ### Arguments: 
 /// * `shifted_wavelengts` - A reference to a [Vec<f64>] that contains the doppler shifted observed wavelengths.
@@ -401,3 +336,123 @@ fn construct_new_vec_using_indices(
     }
     filtered_vector
 }
+
+
+//--------------------------------------------------
+//-----Extracting Intensity and continuum fluxes----
+//--------------------------------------------------
+
+/// This function extracts the intensity
+/// 
+/// ### Arguments: 
+/// * `name` - file name of the intensity grid file 
+/// * `shifted_wavelengths` - collection of doppler shifted wavelenghts from where intensity shall be queried.
+/// * `coschi` - projection of the surface cell normal onto the observer's unit position vector;
+/// ### Returns: 
+/// `(Flux, Continuum, wavelength_vec)` - a tupple containing three vectors of type `f64` where
+/// `Flux` - Is the intensity flux
+/// `Continuum` - continuus intensity flux
+/// `wavelength_vec` - is the wavelength values where the intensities are computed.
+pub fn get_flux_continuum(grid_filename:String,
+    shifted_wavelengths:&[f64],
+    coschi:f64,
+    start_computation:&std::time::Instant)->Option< (Vec<f64> , Vec<f64>, Vec<f64>)>{
+    if let Ok(lf) = read_intensity_grid_file(&grid_filename){
+
+        // if the number of flux points to be calculated is really small, it's useful to filter out some of the wavelengths from the data frame so that the queries are performed faster.
+        let lf_in_range = match filter1_if_contains_wavelenghts(shifted_wavelengths){
+            Some(relevant_range) => {lf.filter(relevant_range)}
+            None => {lf}
+        };
+        let df_unfiltered = lf_in_range.collect().unwrap();
+        let df_filtered = extract_relevant_wavelengths(shifted_wavelengths, df_unfiltered);
+        let lf_relevant = df_filtered.lazy();
+
+        /*lf;match shifted_wavelengths.len() < (N_FLUX_POINTS/100) as usize{
+            true => {
+                lf
+                .filter(filter1_if_contains_wavelenght(&shifted_wavelengths,
+                0.2).unwrap())}
+            false => {lf}
+        };*/
+
+        // Here the lazy frame is transformed into a data frame that contains three columns: the intensity fluxes (specific, continuous) and the relevant wavelengths.
+        let df_flux = create_lf_wavelength_flux_continuum(lf_relevant,coschi).collect().unwrap();
+
+        let lbd_from_df = extract_column_as_vectorf64("wavelength", &df_flux);
+        let flux_from_df = extract_column_as_vectorf64("flux", &df_flux);
+        let continuum_from_df = extract_column_as_vectorf64("continuum", &df_flux);
+
+        Some(( flux_from_df,
+            continuum_from_df,
+            lbd_from_df))
+    }else{
+    None
+    }
+}
+
+/// This function receives a lazy frame containing the coefficients of the limb
+/// darkening law
+/// $I_{\lambda}(\mu) =a_0 \sum_{k=1}^{3} a_k \left( 1 - \mu^k  \right)$
+/// and returns another lazy frame with the wavelengths and their intensity fluxes.
+///
+/// ### Arguments:
+/// * `lf` - A lazy frame read from the intensity grid file
+/// * `coschi` - a `f64` value that is the projection of the unit surface normal onto the unit vector towards the observer.
+/// ### Returns:
+/// * `LazyFrame` - The column headers of this lazy frame are given by 
+/// * `|wavelength|flux|continuum|`
+ fn create_lf_wavelength_flux_continuum(lf:LazyFrame, coschi:f64)->LazyFrame{
+    let mu = coschi.sqrt();
+
+    let bcoef = 1.0 - mu; 
+    let ccoef = 1.0 - coschi;
+    let dcoef = 1.0-mu.powi(3);
+
+    let i_lambda = col("a") 
+        + col("b") * lit(bcoef) 
+        + col("c") * lit(ccoef)
+        + col("d") * lit(dcoef);
+
+    let i_cont = col("ac") 
+        + col("bc") * lit(bcoef) 
+        + col("cc") * lit(ccoef)
+        + col("dc") * lit(dcoef);
+    
+    lf.select([
+        col("wavelength"),i_lambda.alias("flux"),
+        i_cont.alias("continuum")
+    ])
+}
+/// This function selects the four points closer to
+/// (temperature,Log_g)
+/// on the parameter space whose coordinates have an associated intensity grid file
+/// 
+/// ### Arguments:
+/// * `grid_id_lf` - A polars LazyFrame that contains the information of the intensity grids.
+/// * `temperature`` - a `f64` value for the temperature on a surface cell
+/// * `log g`- a `f64` value for the logarithm of gravity on a surface cell
+/// ### Returns:
+/// * `(temperature_vec, log_g_vec, Filenames_vec)` - This is a tuple that contains three vectors where
+/// * `temperature vec` - is a vector of `f64` that contains 4 ordered temperatures of the coordinates in the parameter space
+/// * `log_g_vec` - is a vector of `f64 that contains 4 ordered values of log_g of the coordinates in the parameter space
+/// * `Filenames_vec` - is a vector of `String` that contains the paths for the 4 intensity grid files.
+pub fn get_temp_logg_filenames(grid_id_lf:LazyFrame,
+    temperature:f64,
+    log_gravity:f64)->(Vec<f64>,Vec<f64>,Vec<String>){
+    let lf_relevant = define_parameter_space::get_rectangles_in_parameter_space(grid_id_lf, temperature, log_gravity);
+    let df_relevant = lf_relevant.collect().unwrap();
+
+    let temperature_vec = extract_column_as_vectorf64("temperature", &df_relevant);
+    let log_g_vec = extract_column_as_vectorf64("log_gravity", &df_relevant);
+    let name_of_files = extract_column_as_vector_string("file name", &df_relevant);
+
+    (temperature_vec,log_g_vec , name_of_files)
+}
+// 1) Get the maximum doppler shift value, temperature, and log_g
+// 2) Open and filter all of the relevant grid files
+// 3) rewrite the methods herebelow so that queries and column operations are carried on lazyframes related to this df
+// 4) do some benchmark
+// 5) If nothing works, have a meeting with Joris about How to improve speed. Might be necessary to look into parallelization. 
+
+//pub fn load_gridfiles(temperature:&[f64],)
