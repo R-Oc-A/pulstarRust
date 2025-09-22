552437f5aad51d7b3b8217dedbf7ddabaa783a16
Ricardo Ochoa Armenta <ricardo.ochoaarmenta@kuleuven.be>
2025-09-22 11:22:43 +0200

    Uploading a commented version of the current profile binary that performs faster than using data frames. It will still be necessary to later drop the nested vectors version of the limb darkening coefficients to improve speed

--- profile/src/intensity/extract_intensity_fluxes.rs
+++ profile/src/intensity/extract_intensity_fluxes.rs
@@ -2,6 +2,11 @@ use super::*;
 
 
 impl GridsData {
+    /// This function  computes the specific intensities on a grid using it's limb darkening coefficients
+    /// ### Arguments: 
+    /// `cell` - A &[SurfaceCell] that has the projection angle with the line of sight `coschi`.
+    /// ### Returns: 
+    /// This function doesn't return anything but it updates the flux and continuum vectors contained in the [GridsData] instance. 
     pub fn compute_flux_en_continuum(&mut self,cell: &SurfaceCell){
         let mu = cell.coschi.sqrt();
 
@@ -26,10 +31,9 @@ impl GridsData {
             }
         }
     }
-
+    /// This function is used to store for the observed wavelength the indices of the wavelengths in [GridsData] that will be used for interpolation. 
+    /// This function relies on the bisection algorithm to perform the query.
     pub fn extract_important_rows(&mut self,global_flux: &mut FluxOfSpectra){
-
-        
         self.row_indices.fill(0);
 
         let mut counter = 0usize;
@@ -40,177 +44,5 @@ impl GridsData {
         }
     }
 }
-
-
-/// This is a function that extracts only the relevant wavelengths as [Vec<f64>]. 
-/// 
-/// ### Arguments: 
-/// * `shifted_wavelengts` - A reference to a [Vec<f64>] that contains the doppler shifted observed wavelengths.
-/// * `grid_dataframe` - A [DataFrame] that contains the wavelengths from the grid file. This DataFrame has exluded all the wavelengths outside of range. 
-/// ### Returns: 
-/// * `relevant_df` - A [DataFrame] that only contains the wavelengths that'll be used for the [interpolate] step.
-pub fn extract_relevant_wavelengths(
-    shifted_wavelengths:&[f64],
-    grid_dataframe:&DataFrame)->DataFrame{
-    
-    // Extract df's collumns into vectors; Wavelengths are ordered from lower to greater so we'll profit from that.
-        let grid_precision = 5.0e-3;
-        let wavelength_precision = (shifted_wavelengths[1]-shifted_wavelengths[0]).abs();
-    let relevant_df = match wavelength_precision>grid_precision{// If the requested wavelength spacing is bellow the grid precision, then all of the grids wavelengths are relevant ones.
-        true => {
-            let df_wavelengths=extract_column_as_vectorf64("wavelength", grid_dataframe);
-            let df_a = extract_column_as_vectorf64("a", grid_dataframe);
-	        let df_b = extract_column_as_vectorf64("b", grid_dataframe);
-	        let df_c = extract_column_as_vectorf64("c", grid_dataframe);
-	        let df_d = extract_column_as_vectorf64("d", grid_dataframe);
-	        let df_ac = extract_column_as_vectorf64("ac", grid_dataframe);
-	        let df_bc = extract_column_as_vectorf64("bc", grid_dataframe);
-	        let df_cc = extract_column_as_vectorf64("cc", grid_dataframe);
-	        let df_dc = extract_column_as_vectorf64("dc", grid_dataframe);
-		    // Get relevant indices of the df_wavelength vector into an index vector
-	        let indices = extract_important_indices(shifted_wavelengths, &df_wavelengths);
-    	    // Construct new vectors using only the relevant indices
-	        let wavelengths = construct_new_vec_using_indices(df_wavelengths, &indices);
-	        let a = construct_new_vec_using_indices(df_a, &indices);
-	        let b = construct_new_vec_using_indices(df_b, &indices);
-	        let c = construct_new_vec_using_indices(df_c, &indices);
-	        let d = construct_new_vec_using_indices(df_d, &indices);
-	        let ac = construct_new_vec_using_indices(df_ac, &indices);
-	        let bc = construct_new_vec_using_indices(df_bc, &indices);
-	        let cc = construct_new_vec_using_indices(df_cc, &indices);
-	        let dc = construct_new_vec_using_indices(df_dc, &indices);
-	
-	        // Construct a data frame using the relevant indices
-		    df![
-		        "wavelength"=>wavelengths,
-		        "a" => a,
-		        "b" => b,
-		        "c" => c,
-		        "d" => d,
-		        "ac" => ac,
-		        "bc" => bc,
-		        "cc" => cc,
-		        "dc" => dc
-		    ].unwrap()
-	        }
-	    false => {
-	            grid_dataframe.clone()
-	        }
-	    };
-	    relevant_df
-}
-
-/// This function is useful to get the relevant indices out of the wavelength vector. 
-///
-/// ### Arguments:
-/// * `shifted_wavelengts` - A reference to a [Vec<f64>] that contains the doppler shifted observed wavelengths.
-/// * `df_wavelengts` - A reference to a [Vec<f64>] that contains the grid's wavelengths extracted from the grid [DataFrame].
-/// ### Returns:
-/// * `indices` - A [Vec<usize>] that contains the indices of the df_wavelengths that'll be used for the [interpolate] step.
-fn extract_important_indices(
-    shifted_wavelengths:&[f64],
-    df_wavelengths: &[f64]
-)->Vec<usize>{
-    let mut indices:Vec<usize> = Vec::new();
-    for wavelength in shifted_wavelengths.iter(){
-        let index = search_geq(df_wavelengths, *wavelength);
-        //extract last index
-        /*if let Some(last_index)= indices.last(){
-            if df_wavelengths[*last_index]!=df_wavelengths[index-1]{
-                indices.push(index-1)
-            }
-        }else{
-            indices.push(index-1);
-        }*/
-        indices.push(index-1);
-        indices.push(index);
-    }
-    indices
-}
-
-/// This function filters a vector using a collection of indices
-/// 
-/// ### Arguments: 
-/// * `original_vector` - A [Vec<f64>] that contains the unfiltered data from the grid files
-/// * `index_collection` - A [ &[usize] ] that contains the relevant indices.
-/// ### Returns:
-/// * `filtered_vector` - A [Vec<f64>] that contains the filtered data from the grid files. 
-fn construct_new_vec_using_indices(
-    original_vector:Vec<f64>,
-    index_collection:&[usize]
-)->Vec<f64>{
-    let mut filtered_vector:Vec<f64>= Vec::new();
-    for index in index_collection.iter(){
-        filtered_vector.push(original_vector[*index]);
-    }
-    filtered_vector
-}
-
-/// This function receives a lazy frame containing the coefficients of the limb
-/// darkening law
-/// $I_{\lambda}(\mu) =a_0 \sum_{k=1}^{3} a_k \left( 1 - \mu^k  \right)$
-/// and returns another lazy frame with the wavelengths and their intensity fluxes.
-///
-/// ### Arguments:
-/// * `lf` - A lazy frame read from the intensity grid file
-/// * `coschi` - a `f64` value that is the projection of the unit surface normal onto the unit vector towards the observer.
-/// ### Returns:
-/// * `LazyFrame` - The column headers of this lazy frame are given by 
-/// * `|wavelength|flux|continuum|`
- fn create_lf_wavelength_flux_continuum(lf:LazyFrame, coschi:f64)->LazyFrame{
-    let mu = coschi.sqrt();
-
-    let bcoef = 1.0 - mu; 
-    let ccoef = 1.0 - coschi;
-    let dcoef = 1.0 - mu.powi(3);
-
-    let i_lambda = col("a") 
-        + col("b") * lit(bcoef) 
-        + col("c") * lit(ccoef)
-        + col("d") * lit(dcoef);
-
-    let i_cont = col("ac") 
-        + col("bc") * lit(bcoef) 
-        + col("cc") * lit(ccoef)
-        + col("dc") * lit(dcoef);
-    
-    lf.select([
-        col("wavelength"),i_lambda.alias("flux"),
-        i_cont.alias("continuum")
-    ])
-}
-
-/// This function extracts the intensity
-/// 
-/// ### Arguments: 
-/// * `name` - file name of the intensity grid file 
-/// * `shifted_wavelengths` - collection of doppler shifted wavelenghts from where intensity shall be queried.
-/// * `coschi` - projection of the surface cell normal onto the observer's unit position vector;
-/// ### Returns: 
-/// `(Flux, Continuum, wavelength_vec)` - a tupple containing three vectors of type `f64` where
-/// `Flux` - Is the intensity flux
-/// `Continuum` - continuus intensity flux
-/// `wavelength_vec` - is the wavelength values where the intensities are computed.
-pub fn get_flux_continuum(
-    grid_dataframe: &DataFrame,
-    shifted_wavelengths:&[f64],
-    coschi:f64
-    )->Option< (Vec<f64> , Vec<f64>, Vec<f64>)>{
-
-    let df = extract_relevant_wavelengths(shifted_wavelengths, grid_dataframe);
-
-
-    // Here the lazy frame is transformed into a data frame that contains three columns: the intensity fluxes (specific, continuous) and the relevant wavelengths.
-    let df_flux = create_lf_wavelength_flux_continuum(df.lazy(),coschi).collect().unwrap();
-
-    let lbd_from_df = extract_column_as_vectorf64("wavelength", &df_flux);
-    let flux_from_df = extract_column_as_vectorf64("flux", &df_flux);
-    let continuum_from_df = extract_column_as_vectorf64("continuum", &df_flux);
-
-    Some(( flux_from_df,
-        continuum_from_df,
-        lbd_from_df))
-}
-
 
 
--- profile/src/interpolate.rs
+++ profile/src/interpolate.rs
@@ -2,8 +2,6 @@ use super::*;
 
 impl SurfaceCell{
     pub fn interpolate(& self, grids_data: &mut GridsData,global_flux:&mut FluxOfSpectra){
-    
-
     // Counter orders the intensities 
     let mut counter:usize = 0;
     
@@ -53,7 +51,6 @@ impl SurfaceCell{
             counter += 1;//<- counter should from 0 to 3
         }
     }
-        
     }
 }
 /// Computes by linear interpolation (see [trilinear interpolation](https://paulbourke.net/miscellaneous/interpolation/) ) the specific and continuum intensity for a surface cell.
@@ -73,76 +70,6 @@ impl SurfaceCell{
 /// * `(intens_result,cont_result)` - a tuple that contains two `f64` vectors where intens is the total intensity; cont is the continuum intensity
 /// * `intens_result` - is the collection of specific intensity fluxes that correspond to the observed wavelengths
 /// * `cont_result` - is the collection of the continuum intensity fluxes that correspond to the observed wavelengths
-pub fn interpolate(
-    intensity_dfs:&IntensityDataFrames,
-    relevant_indices:Vec<usize>,
-    shifted_wavelengths:&[f64],
-    flux_vec:&Vec<Vec<f64>>,
-    cont_vec:&Vec<Vec<f64>>,
-    wavelengths_vec:&Vec<Vec<f64>>,  
-    temperature:f64,
-    log_g:f64)->(Vec<f64>,Vec<f64>){
-    
-    let mut temperature_vec:Vec<f64>=Vec::new();
-    let mut log_gravity_vec:Vec<f64>=Vec::new();
-    for index in relevant_indices.iter(){
-        temperature_vec.push(intensity_dfs.temperature_vector[*index]);
-        log_gravity_vec.push(intensity_dfs.log_g_vector[*index]);
-    }
-    // Counter orders the intensities 
-    let mut counter:usize = 0;
-    
-    let t0 = (temperature-temperature_vec[0])/(temperature_vec[2]-temperature_vec[0]);
-    let t1 = (log_g-log_gravity_vec[0])/(log_gravity_vec[1]-log_gravity_vec[0]);
-    
-    //Initialize the collecting vectors.
-    let mut interpolated_flux = vec![0.0;shifted_wavelengths.len()];
-    let mut interpolated_cont= vec![0.0;shifted_wavelengths.len()];
-    
-    // loop over the four elements of the (temperature, log_gravity) parameter space
-    for i in 0..=1{//<- i goes from 0 to 1
-        let delta_temp = match i {
-            0 => 1.0-t0, 
-            1 => t0,
-            _ =>{0.0}};//although verbose, computationally this is faster than multiplying floats
-
-        for j in 0..=1{//<- j goes from 0 to 1
-            let delta_logg = match j{
-                0 => {1.0-t1}
-                1 => {t1}
-                _=> {0.0}};
-
-            for (n,lambda) in shifted_wavelengths.iter().enumerate(){//<-this is to carry the same process onto every wavelength
-
-                let index = search_geq(&wavelengths_vec[counter], *lambda);
-                let t2 = (lambda - wavelengths_vec[counter][index-1])/
-                (wavelengths_vec[counter][index]-wavelengths_vec[counter][index-1]);
-                
-                for k in 0..=1{
-                    let delta_lambda = match k {
-                        0 => {1.0-t2}
-                        1 => {t2}
-                        _ => {0.0} };
-             
-                    interpolated_flux[n] += 
-                    get_contribution_from_vertex(delta_temp,
-                        delta_logg,
-                        delta_lambda,
-                        flux_vec[counter][index-1]);
-             
-                    interpolated_cont[n] += 
-                    get_contribution_from_vertex(delta_temp,
-                        delta_logg,
-                        delta_lambda,
-                        cont_vec[counter][index-1]);
-                }
-            }
-            counter += 1;//<- counter should from 0 to 3
-        }
-    }
-
-    (interpolated_flux,interpolated_cont)    
-}
 
 /// The trilinear interpolation estimates a quantity that depends on position on a point inside of a cube where the
 /// values are known on each of the vertices. This function returns the contribution given by the value on a particular vertex.
